69d8c874ef8166b7f0a36ee52e9aac7526db0e56
Tariq Pathan <tariq047@gmail.com>
2023-09-23 12:05:53 +0100

    bit of cleanup

--- extraction_engine/managers/config_manager.py
+++ extraction_engine/managers/config_manager.py
@@ -1,76 +1,42 @@
 import os
-import json
+from typing import Dict, Optional
+import yaml
 from pathlib import Path
-from extraction_engine.managers.file_manager import FileManager
+from file_manager import FileManager
 
 class ConfigManager:
-    """
-    Manages the configuration files for the application.
-    CONFIG_BASE_PATH: Name of the directory where the config files are stored.
-    Default directory structure: root_path/config
-    """
-    CONFIG_BASE_PATH = FileManager.construct_path("config")
-    _instance = None  # The single instance of ConfigManager
-    paths = {}  # Class-level attribute for paths
+    _instance = None
+    _config = None
 
-    @classmethod
-    def get_instance(cls):
-        if cls._instance is None:
-            cls._instance = cls()
-            cls.paths = cls._load_paths()
-            cls._initialize_config_paths()
+    def __new__(cls):
+        if not cls._instance:
+            cls._instance = super().__new__(cls)
+            config_path = cls._get_file_path()
+            cls._instance._load_config(config_path)
         return cls._instance
 
-    @staticmethod
-    def _load_paths():
-        path = FileManager.construct_path('/paths.json')
-        try:
-            with path.open(encoding='utf-8') as file:
-                return json.load(file)
-        except FileNotFoundError:
-            raise ValueError("paths.json not found.")
-        except json.JSONDecodeError:
-            raise ValueError("Error decoding paths.json. Ensure it's valid JSON.")
+    @classmethod
+    def _get_file_path(cls, filepath: str='config') -> Path:
+        return FileManager.get_filepaths(filepath)
+
+    def _load_config(self, file: Path) -> Dict:
+        with open(file, 'r') as config_file:
+            self._config = yaml.safe_load(config_file)
+        return self._config
 
     @classmethod
-    def _get_path_from_env_or_default(cls, filename: str, env_var: str) -> Path:
-        """
-        Resolves the path for a given filename, using the environment variable if it exists.
-        Expected environment variables: CONFIG_PATH, COVERPAGE_SETTINGS_PATH
-        """
-        default_path = FileManager.construct_path(filename)
-        return Path(os.environ.get(env_var, default_path))
-
-    def __init__(self):
-        if self._instance:
-            return  # Avoid reinitializing if an instance already exists
-
-    @classmethod
-    def _initialize_config_paths(cls):
-        cls._CONFIG_PATHS = {}
-        for key, name in cls.paths.items():
-            cls._CONFIG_PATHS[key] = FileManager.construct_path(CONFIG_BASE_PATH, name)
-
-    def _load_config(self, config_type: str):
-        """Loads the config file based on its type and returns the config as a dictionary."""
-        path = self._CONFIG_PATHS.get(config_type)
-        if not path:
-            raise ValueError(f"Unknown config type: {config_type}")
-
-        try:
-            with open(path, 'r') as file:
-                return json.load(file)
-        except FileNotFoundError:
-            raise ValueError(f"Config file {path} not found.")
-        except json.JSONDecodeError:
-            raise ValueError("Error decoding the config file. Ensure it's valid JSON.")
-
-    def get_config(self, config_type: str, exam_format: str) -> dict:
-        """Returns the configuration for a specific exam board."""
-        config = self._load_config(config_type)
-        board_config = config.get(exam_format)
-
-        if not board_config:
-            raise ValueError(f"No configuration found for exam board: {exam_format}")
-
-        return board_config
+    def get_config(cls, exam_format, config_type=None):
+        if not cls._instance:
+            cls._instance = cls()
+        if cls._instance._config is None:
+            raise ValueError("Configuration has not been loaded.")
+        if config_type:
+            return cls._instance._config.get(config_type, {}).get(exam_format)
+        return cls._instance._config.get(exam_format)
+    
+    # Uncomment this block if you need to get paths from the loaded config
+    # @classmethod
+    # def get_path(cls, path_type):
+    #     if not cls._instance:
+    #         cls._instance = cls()
+    #     return cls._instance._config.get('paths', {}).get(path_type)
